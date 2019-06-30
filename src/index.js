94219b0d17881ef998ba0d83566c0f04696db969
MrGagi <dragan1176@gmail.com>
2019-06-30 11:48:35 +0200

    Get rid of node sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles/app.scss';
+import './styles/app.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
